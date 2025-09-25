9907610f094d1a85a9fd6e236e09a5cda21ecf88
sjohn198 <sjohn198@calpoly.edu>
2025-09-25 14:45:57 -0700

    working on getText

--- debugassist/src/extension.ts
+++ debugassist/src/extension.ts
@@ -6,18 +6,29 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "debugassist" is now active!');
+	let disposable = vscode.commands.registerCommand('debugAssist.getText', () => {
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			const document = editor.document;
+			const text = document.getText();
+
+			vscode.window.showInformationMessage("Successfully captured the text!");
+
+			console.log(text);
+		} else {
+			vscode.window.showWarningMessage("No editor is open");
+		}
+	});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('debugassist.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from debugassist!');
-	});
+	// const disposable = vscode.commands.registerCommand('debugassist.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from debugassist!');
+	// });
 
 	context.subscriptions.push(disposable);
 }

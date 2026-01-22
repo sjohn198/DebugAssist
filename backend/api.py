a65e7c0fc15005a14baf93cb48e4d855443e1346
sjohn198 <sjohn198@calpoly.edu>
2026-01-22 11:52:03 -0800

    revert to gemini

--- backend/api.py
+++ backend/api.py
@@ -1,16 +1,15 @@
 import os
 import json
+from google import genai
+from google.genai import types
 from openai import OpenAI, APIConnectionError
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-OLLAMA_URL = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434/v1")
+load_dotenv()
 
-client = OpenAI(
-    base_url=OLLAMA_URL,
-    api_key="ollama"
-)
+client = genai.Client()
 
 app = FastAPI()
 
@@ -22,9 +21,10 @@ class Selection(BaseModel):
     style: bool
 
 class BasicOutput(BaseModel):
-    number_of_errors: int
+    error_type: str
     line_number: int
-    line_text: str
+    character_number: int
+    error_message: str
 
 @app.post("/api/test-extension-connection")
 async def connect_extension(selection: Selection):
@@ -36,48 +36,45 @@ async def connect_extension(selection: Selection):
 async def test_openai(selection: Selection):
     print(selection)
     try:
-        response = client.chat.completions.create(
-            model="hf.co/LiquidAI/LFM2-1.2B-RAG-GGUF",
-            response_format={"type": "json_object"},
-            messages=[
-                {
-                    "role": "system", 
-                    "content": f"""
-                    You are an expert code debugger and tutor. You do not write code. You only analyze input and output raw JSON. 
-                    {"You will critique programming errors." if selection.errors else ""}
-                    {"You will recommend optimization improvements." if selection.optims else ""}
-                    {"You will recommend coding style improvements." if selection.style else ""}
-                    
-                    Do not include markdown formatting, code blocks, or conversational text. Output ONLY the JSON object. 
-                    
-                    I will give you code and the intended function of the code. Please respond with a json object with the 
-                    following fields:
-                    
-                    1. error_type: The standard name of the error (e.g., NameError, SyntaxError).
-                    2. line_number: Just the integer number.
-                    3. character_number: Just the integer number.
-                    4. error_message: A verbose and helpful explanation. You must explain WHY the error occurred in this specific context and suggest HOW to fix it. Avoid generic compiler messages.
-                    """ 
-                },
-                {
-                    "role": "user",
-                    "content": selection.code
-                },
-                {
-                    "role": "user",
-                    "content": selection.prompt
-                }
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=[
+                f"""
+                <code_to_analyze>
+                {selection.code}
+                </code_to_analyze>
+                """,
+                f"""
+                <user_instruction>
+                {selection.prompt}
+                </user_instruction>
+                """
             ],
-            temperature=0.2
+            config=types.GenerateContentConfig(
+                system_instruction = f"""
+                You are an expert code debugger and tutor. You do not write code. You only analyze input and output raw JSON. 
+                {"You will critique programming errors." if selection.errors else ""}
+                {"You will recommend optimization improvements." if selection.optims else ""}
+                {"You will recommend coding style improvements." if selection.style else ""}
+                
+                Do not include markdown formatting, code blocks, or conversational text. Output ONLY the JSON object. 
+                
+                I will give you code and the intended function of the code. Please respond with a json object with the 
+                following fields:
+                
+                1. error_type: The standard name of the error (e.g., NameError, SyntaxError).
+                2. line_number: Just the integer number.
+                3. character_number: Just the integer number.
+                4. error_message: A verbose and helpful explanation. You must explain WHY the error occurred in this specific context and suggest HOW to fix it. Avoid generic compiler messages.
+                """,
+                temperature=0.2,
+                response_mime_type="application/json",
+                response_schema = BasicOutput
+            )
         )
     except Exception as e:
         print(f"\nAn error occurred: {e}")
         response = ""
-    print(f"AI response: {response.choices[0].message.content}")
-    raw_text = response.choices[0].message.content
-    print(f"Raw text: {raw_text}")
-    messages = json.loads(raw_text)
-    print(messages)
-    print(messages["error_message"])
-    output = messages["error_message"]
-    return output
+    print(f"Response: {response}")
+    print(f"Content: {response.parsed.error_message}")
+    return response.parsed.error_message
